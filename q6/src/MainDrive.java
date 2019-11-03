45848f8f6e74f783b9ffca8bc03e672a77e04702
Terry Funggg <terryyessfung@gmail.com>
2019-11-03 12:00:53 +0800

    ListStack

--- q6/src/MainDrive.java
+++ q6/src/MainDrive.java
@@ -3,30 +3,30 @@ import linkedList.EmptyListException;
 public class MainDrive {
 	public static void main(String[] args) {
 		try {
-			ListStack stack = new ListStack();
-			stack.push(10);
-			stack.push(15);
-			stack.push("Apple");
-			stack.push("Orange");
-			stack.push(100);
-			
-			System.out.println(stack.toString());
-			
-			stack.pop();
-			stack.pop();
-			System.out.println(stack.toString());
-			stack.pop();
-			stack.pop();
-			stack.pop();
-			stack.pop();
-			System.out.println(stack.toString());
+				ListStack stack = new ListStack();
+				stack.push(10);
+				stack.push(15);
+				stack.push("Apple");
+				stack.push("Orange");
+				stack.push(100);
+				
+				System.out.println(stack.toString());
+				
+				stack.pop();
+				stack.pop();
+				System.out.println(stack.toString());
+				stack.pop();
+				stack.pop();
+				stack.pop();
+				stack.pop();
+				System.out.println(stack.toString());
 			
 			
 		} catch (EmptyListException e) {
 			System.out.println(e.getMessage());
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("Unexpected error: " + e.getMessage());
 		}
 		
 		}
