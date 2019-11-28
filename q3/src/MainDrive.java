4eb45867f9e1f17c7b19bcc30fff2a1c8b31c39e
TerryFunggg <terryyessfung@gmail.com>
2019-11-28 15:55:20 +0800

    harmonic

--- q3/src/MainDrive.java
+++ q3/src/MainDrive.java
@@ -5,18 +5,35 @@
  */
 
 public class MainDrive {
-	double sum = 0;
+	private double sum = 0;
 	
 	public static void main(String[] args) {
 		MainDrive app = new MainDrive();
-		app.harmonic(83);
-		System.out.println(app.sum);
+		
+		app.recursiveHarmonic(10);
+		System.out.println("RecursiveHarmonic = " + app.getSum());
+		
+		//app.harmonic(150);
 	}
 		
-	void harmonic(int n) {
-		if(n == 0) return;
+	void recursiveHarmonic(double n) {
+		if(n <= 0) return;
 		this.sum += (1.0/n);
 
-		harmonic(n - 1);
+		recursiveHarmonic(n - 1);
+	}
+	
+	public double getSum() {
+		return sum;
+	}
+	
+	void harmonic(double d) {
+		 sum = 0;
+		for(int i=1;i<=d;i++)
+		{
+			sum=sum+(1d/i);
+			
+		}
+		System.out.println("harmonic = "+sum);
 	}
 }
